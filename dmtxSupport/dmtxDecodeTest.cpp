49547a7b1eff1e472ee9ba11a40c96da3b5de0eb
jwlodek <jwlodek@cs.stonybrook.edu>
2019-03-29 16:12:03 -0400

    add some comments to example

--- dmtxSupport/dmtxDecodeTest.cpp
+++ dmtxSupport/dmtxDecodeTest.cpp
@@ -1,5 +1,4 @@
 /**
- * 
  * Test program for decoding sample image with dmtx code
  * 
  * Author: Jakub Wlodek
@@ -18,24 +17,31 @@ using namespace std;
 
 int main(int argc, char** argv){
 
+    // libdmtx structures
     DmtxImage* dmtxImage;
     DmtxDecode* dmtxDecode;
     DmtxRegion* dmtxRegion;
     size_t bytesPerPixel = 24;
     DmtxMessage* message;
 
+    // read in test image with opencv
     Mat im = imread("DMTXTest.png");
 
+    // initialize the image for dmtx
     dmtxImage = dmtxImageCreate(im.data, im.size().width, im.size().height, DmtxPack24bppRGB);
     assert(dmtxImage != NULL);
 
+    // initialize the decoder
     dmtxDecode = dmtxDecodeCreate(dmtxImage, 1);
     assert(dmtxDecode != NULL);
 
+    // find the region in which the data matrix code is found
     dmtxRegion = dmtxRegionFindNext(dmtxDecode, NULL);
     if(dmtxRegion != NULL){
+        // if a region is found, decode it
         message = dmtxDecodeMatrixRegion(dmtxDecode, dmtxRegion, DmtxUndefined);
         if(message != NULL){
+            // print to stdout
             fputs("output: \"", stdout);
             fwrite(message->output, sizeof(unsigned  char),  message->outputIdx, stdout);
             fputs("\"\n", stdout);
@@ -44,6 +50,7 @@ int main(int argc, char** argv){
         dmtxRegionDestroy(&dmtxRegion);
     }
 
+    // memory cleanup
     dmtxDecodeDestroy(&dmtxDecode);
     dmtxImageDestroy(&dmtxImage);
     im.release();
